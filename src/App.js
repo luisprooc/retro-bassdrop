4c396c3871e49919fb0d34a5f8c1caebfe484316
Luis <luisprooc@gmail.com>
2021-02-03 09:22:51 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+const App = () => {
+  return(
+    <div className="container">
+      <h1 className="py-5 text-center">RETRO BASS DROP</h1>
+      <div className="d-flex justify-content-center flex-column align-items-center">
+        
+      </div>
     </div>
   );
-}
+};
+
 
 export default App;
---
+++ src/components/cube.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Cube = () => {
+    return(
+        <>
+        </>
+    );
+};
+
+export default Cube;
+
